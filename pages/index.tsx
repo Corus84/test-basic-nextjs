3a4bfead46876963ea654609bfcf6f35e11c6f75
Corus84 <nacho@devaway.es>
2021-10-06 22:23:31 +0200

    feat: pre-rendering example

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,23 @@ import Head from 'next/head'
 import Link from 'next/link';
 import Layout, { SITE_TITLE } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+      currentDate: new Date().toTimeString()
+    }
+  }
+}
+
+export default function Home({ allPostsData, currentDate }) {
   return (
     <Layout home>
       <Head>
-        <title>{ SITE_TITLE }</title>
+        <title>{SITE_TITLE}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
@@ -17,11 +28,28 @@ export default function Home() {
         </p>
       </section>
       <h1 className="title">
-          Read{' '}
-          <Link href="/posts/first-post">
-            <a>this page!</a>
-          </Link>
-        </h1>      
+        Link to{' '}
+        <Link href="/posts/first-post">
+          <a>other page!</a>
+        </Link>
+      </h1>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+              <br/>
+              Current date: { currentDate }
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
 }
